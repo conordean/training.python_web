38ea0a1619fec249c3bafac7cd27aea6d1c69f22
Conor Heathcote <conordean@gmail.com>
2013-01-15 17:36:47 -0800

    This is the updated file

--- assignments/week01/lab/echo_client.py
+++ assignments/week01/lab/echo_client.py
@@ -1,16 +1,48 @@
-import socket
+#! /usr/bin/python
+
 import sys
+import socket
 
-# Create a TCP/IP socket
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 50000)
+class EchoClient(object):
+  """Interacts with Server, sending and recieving request data."""
 
-try:
-    # Send data
-    message = 'This is the message.  It will be repeated.'
+  def __init__(self, host='127.0.0.1', port=50000):
+    """Constructs the socket connection, configures the client."""
+  
+    # Create a TCP/IP socket
+    self.client = socket.socket(2, 1, 0)   
+ 
+    # Connect the socket to the port where the server is listening
+    self.client.connect((host, port))
 
-    # print the response
+  def MakeRequest(self, request):
+    # Send data to server
+    self.client.sendall(request)
 
-finally:
-    # close the socket to clean up
+  def UserInput(self): 
+    value = raw_input("Add, Subtract, Multiply, or Divide two integers. (4 + 4) ")
+    return value
+
+  def FetchResult(self):
+    result = self.client.recv(1024)
+    return result
+  
+  def CloseSocket(self):
+    return self.client.close()
+
+
+def main():
+  while True:
+    echo_client = EchoClient()
+    message = echo_client.UserInput()
+    try:
+      # send data
+      echo_client.MakeRequest(message)
+      # print the response
+      print echo_client.FetchResult()
+    finally:
+      print "Closing Socket"
+      echo_client.CloseSocket()
+
+main() 
