0fc59a2da8c7ad7cf5793efb879a1312628f3f3f
Conor Heathcote <conordean@gmail.com>
2013-01-15 17:37:02 -0800

    This is the updated file

--- assignments/week01/lab/echo_server.py
+++ assignments/week01/lab/echo_server.py
@@ -1,19 +1,60 @@
-import socket
+#! /usr/bin/python
+
 import sys
+import socket
 
-# Create a TCP/IP socket
 
-# Bind the socket to the port
-server_address = ('localhost', 50000)
+class EchoServer(object):
+  """Interacts with Client, recieving and sending data."""
 
-# Listen for incoming connections
+  def __init__(self, host="127.0.0.1", port=50000, backlog=5):
+    """Constructs the socket connection, configures the server."""
 
-while True:
+    # Create a TCP/IP socket
+    self.server = socket.socket(2, 1, 0)
+    # Bind the socket to the port
+    self.server.bind((host, port))
+    print 'Server started on port: ', port
+    # Listen for incoming connections
+    self.server.listen(backlog)
+    print("Server listening\n")
+
+  def ProcessRequest(self):
     # Wait for a connection
+    self.con, self.cli = self.server.accept()
+    print 'New connection from ', self.cli
+    self.data = self.con.recv(1024)
+    print "Client Request: " + self.data
+    return self.data
+    
+  def ProcessResponse(self, response):
+    # Send data to client
+    self.con.sendall('The result is ' + str(response))
 
+  def DoMath(self, data):
+    self.data = self.data.split()
+    if self.data[1] == '+':
+      result = int(self.data[0]) + int(self.data[2])
+    elif self.data[1] == '-':
+      result = int(self.data[0]) - int(self.data[2])
+    elif self.data[1] == '*':
+      result = int(self.data[0]) * int(self.data[2])
+    elif self.data[1] == '/':
+      result = int(self.data[0]) / int(self.data[2])
+    return result
+
+  def CloseSocket(self):
+    self.con.close()
+
+
+def main():
+  echo_server = EchoServer()
+  while True:
+    data = echo_server.ProcessRequest()
     try:
-        # Receive the data and send it back
-        
-
+      echo_server.ProcessResponse(echo_server.DoMath(data))
     finally:
-        # Clean up the connection
+      print "Closing Socket"
+      echo_server.CloseSocket()
+
+main()
